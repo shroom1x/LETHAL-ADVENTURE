8b7d5e01d799b8a4d57f205b17bc98e51cba8060
shroom1x <shrmxmak@gmail.com>
2026-05-29 16:09:56 +0300

    Updated

---
+++ LA_Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace LA_Changeloger
+{
+    public static class LA_Logger
+    {
+        private static string logFilePath;
+
+        static LA_Logger()
+        {
+            try
+            {
+                string assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                string logsFolderPath = Path.Combine(assemblyDir, "logs");
+
+                if (!Directory.Exists(logsFolderPath))
+                {
+                    Directory.CreateDirectory(logsFolderPath);
+                }
+
+                logFilePath = Path.Combine(logsFolderPath, "log.txt");
+
+                File.WriteAllText(logFilePath, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogError($"[LALogger] Не удалось создать папку или файл лога: {ex.Message}");
+            }
+        }
+
+        public static void Initialize() { }
+    }
+}
