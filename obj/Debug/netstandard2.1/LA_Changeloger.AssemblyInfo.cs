2d7d931f76d81fa8e6367fc321dad4b8a9b41afe
shroom1x <shrmxmak@gmail.com>
2026-09-12 00:06:50 +0300

    Update .dll

--- obj/Debug/netstandard2.1/LA_Changeloger.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/LA_Changeloger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LA_Changeloger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.3.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3+588b78c8bb43770ead70245434f3c3201e1a16e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3+0424c853d25e61b6a200acfed28f0dcbb998759e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LA_Changeloger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LA_Changeloger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.3.0")]
